d8ea3625824957665e02191ce19f2c55df508167
Ohin-Kyuu <ohin.kyuu@gmail.com>
2025-11-20 18:39:32 +0800

    MODIFIED: use merge install for colcon build

--- docker/scripts/build/colcon_build.sh
+++ docker/scripts/build/colcon_build.sh
@@ -6,6 +6,6 @@ ROS_WS=/home/${USER}/vision_ws
 
 source /opt/ros/humble/setup.bash
 cd ${ROS_WS}
-colcon build
+colcon build --merge-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 chown -R ${USER}:${USER} ${ROS_WS}
 echo "source ${ROS_WS}/install/setup.bash" >> /home/${USER}/.bashrc
