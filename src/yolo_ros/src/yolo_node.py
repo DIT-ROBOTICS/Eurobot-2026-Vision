e4e9f7816e4e531a8438fdf6953b827ad08aedbb
Latuuuuu <bred16641@gapp.nthu.edu.tw>
2026-01-09 19:33:49 +0800

    add : yolo node mode param

--- src/yolo_ros/src/yolo_node.py
+++ src/yolo_ros/src/yolo_node.py
@@ -11,6 +11,7 @@ import tf2_ros
 import message_filters
 import traceback
 import tf2_geometry_msgs
+from rcl_interfaces.msg import SetParametersResult 
 
 class HazelnutDetector(Node):
     def __init__(self):
@@ -47,10 +48,38 @@ class HazelnutDetector(Node):
             [-0.02,  0.02, 0]
         ], dtype=np.float32)
 
+        self.mode_list = ['bb', 'obb_dep', 'obb_pnp', 'obb_mix']
+        self.func_list = [self.bb_to_tf, self.obb_to_tf, self.obb_pnp_to_tf, self.obb_mix_to_tf]
+
+        self.declare_parameter('mode', 'obb_mix')
+        mode_str = self.get_parameter('mode').get_parameter_value().string_value
+        if mode_str in self.mode_list:
+            self.mode = self.mode_list.index(mode_str)
+        else:
+            self.get_logger().error(f"初始模式 '{mode_str}' 無效，設為 {self.mode_list.index('obb_mix')}",)
+            self.mode = self.mode_list.index('obb_mix')
+        self.add_on_set_parameters_callback(self.parameter_callback)
+        self.get_logger().info(f"目前偵測模式: {self.mode}")
+
         self.ts = message_filters.ApproximateTimeSynchronizer([self.rgb_sub, self.dep_sub], queue_size=10, slop=0.1)
         self.ts.registerCallback(self.image_callback)
 
         self.get_logger().info("Hazelnut Detector 已啟動。")
+        
+    def parameter_callback(self, params):
+        result = SetParametersResult()
+        result.successful = True
+
+        for param in params:
+            if param.name == 'mode':
+                if param.value in self.mode_list:
+                    self.mode = self.mode_list.index(param.value)
+                    self.get_logger().info(f"已動態切換至模式: {param.value} (索引: {self.mode})")
+                else:
+                    result.successful = False
+                    result.reason = f"無效模式: {param.value}。可用選項: {self.mode_list}"
+
+        return result
 
     def camera_info_callback(self, msg):
         self.camera_intrinsics = msg
@@ -169,11 +198,60 @@ class HazelnutDetector(Node):
 
             self.broadcast_hazelnut_tf(x_cam, y_cam, z_cam, rgb_msg.header, i)
 
+    def obb_mix_to_tf(self, results, rgb_msg):
+        for i, box in enumerate(results[0].obb):
+
+            # pnp part
+            image_points = box.xyxyxyxy[0].cpu().numpy().astype(np.float32)
+
+            camera_matrix = np.array(self.camera_intrinsics.k).reshape(3, 3)
+            dist_coeffs = np.array(self.camera_intrinsics.d)
+
+            ret, rvec, tvec = cv2.solvePnP(self.aruco_points, image_points, camera_matrix, dist_coeffs)
+
+            if not ret:
+                self.get_logger().warning(f"目標 {i} PnP 求解失敗，跳過。")
+                continue
+
+            x_cam_pnp, y_cam_pnp, z_cam_pnp = tvec.flatten()
+
+            # depth part
+            obb = box.xywhr[0].cpu().numpy()
+            u,v = int(obb[0]), int(obb[1])
+
+            u_min, u_max = max(0, u-2), min(self.dep_img.shape[1], u+2)
+            v_min, v_max = max(0, v-2), min(self.dep_img.shape[0], v+2)
+            depth_roi = self.dep_img[v_min:v_max, u_min:u_max]
+
+            valid_depths = depth_roi[depth_roi > 0]
+            if valid_depths.size == 0:
+                self.get_logger().warning(f"目標 {i} 無有效深度資料，跳過。")
+                continue
+
+            depth = np.median(valid_depths) / 1000.0
+
+            self.get_logger().info(f"目標 {i} 深度: {depth:.2f} m")
+
+            fx = self.camera_intrinsics.k[0]
+            fy = self.camera_intrinsics.k[4]
+            cx = self.camera_intrinsics.k[2]
+            cy = self.camera_intrinsics.k[5]
+
+            z_cam_dep = float(depth)
+            x_cam_dep = (u - cx) * z_cam_dep / fx
+            y_cam_dep = (v - cy) * z_cam_dep / fy
+
+            x_cam = (x_cam_pnp + x_cam_dep) / 2.0
+            y_cam = (y_cam_pnp + y_cam_dep) / 2.0
+            z_cam = (z_cam_pnp + z_cam_dep) / 2.0
+
+            self.broadcast_hazelnut_tf(x_cam, y_cam, z_cam, rgb_msg.header, i)
+
+
     def image_callback(self, rgb_msg, dep_msg):
         if self.camera_intrinsics is None:
             self.get_logger().warning("尚未收到相機內參資訊，無法進行偵測。")
             return
-        
         try:
             self.hazelnut_poses.poses = []
             self.hazelnut_poses.header.frame_id = "map" 
@@ -186,7 +264,7 @@ class HazelnutDetector(Node):
             image = cv2.merge([gray_image, gray_image, gray_image])
             results = self.model(image, conf=0.75, verbose=False, device=0)
 
-            self.obb_to_tf(results, rgb_msg)
+            self.func_list[self.mode](results, rgb_msg)
 
             self.pose_pub.publish(self.hazelnut_poses)
 
